5c258ddc892f83b6d55a8a7cb4942b1e40dd5ada
Toan Vu <toanvu.me@gmail.com>
2015-04-05 22:20:07 +0700

    exercise for connecting to database and CRUD operator

--- class/Database.php
+++ class/Database.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * this class construct mysql pdo driver
+ * should be used for CRUD - CREATE READ UPDATE DELETE - operator
+ * and used for connection
+ */
 class Database{
-	
+    
 }
--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php
+// @TODO : include configuration file and assign it to the variable
+
+// @TODO : include database class
+
+// @TODO : create object instead of the database class and pass the config to its constructor
+
+// @TODO : call a connection and should throw the exception if connect is not established
